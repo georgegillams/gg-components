ca17b811f1d7baeaa0553e51dcbec00449895da4
George Gillams <george.gillams@skyscanner.net>
2019-07-01 21:36:08 +0100

    Reset

--- components/index.js
+++
@@ -1,2 +0,0 @@
-export {default} from './App/App'
-export *  from './GGButton'
--- stories/index.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
-import './App.story'
