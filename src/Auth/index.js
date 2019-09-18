9bb716ab6e951f3e5d20b914f2f8bad16b67fe4d
George Gillams <george.gillams@skyscanner.net>
2019-09-18 17:36:11 +0100

    [NO-JIRA] Add more auth components

--- src/Auth/index.js
+++ src/Auth/index.js
@@ -1,22 +1,24 @@
 import AdminOnly from './AdminOnly';
 import APIEntity from './APIEntity';
-// import DebugObject from './DebugObject';
-// import EmailVerifiedOnly from './EmailVerifiedOnly';
-// import LoadingCover from './LoadingCover';
+import ObjectAsList from './ObjectAsList';
+import DebugObject from './DebugObject';
+import EmailVerifiedOnly from './EmailVerifiedOnly';
+import LoadingCover from './LoadingCover';
 import LoggedInOnly from './LoggedInOnly';
 import LoggedOutOnly from './LoggedOutOnly';
-// import Status from './Status';
-// import User from './User';
+import Status from './Status';
+import User from './User';
 
 export default AdminOnly;
 export {
   APIEntity,
   AdminOnly,
-  //   DebugObject,
-  //   LoadingCover,
-  //   EmailVerifiedOnly,
+  DebugObject,
+  LoadingCover,
+  EmailVerifiedOnly,
   LoggedInOnly,
   LoggedOutOnly,
-  //   Status,
-  //   User,
+  ObjectAsList,
+  Status,
+  User,
 };
