9e7a20131abc94c7f583859534c7c177e90ececb
George Gillams <george.gillams@skyscanner.net>
2020-01-04 22:30:28 +0000

    [NO-JIRA] Add paragraph component

---
+++ src/Typography/Paragraph.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { cssModules } from '../helpers/cssModules';
+
+import STYLES from './typography.scss';
+
+const getClassName = cssModules(STYLES); // REGEX_REPLACED
+
+const Paragraph = props => {
+  const { className, children, ...rest } = props;
+
+  return (
+    <p
+      className={getClassName(
+        'typography__main',
+        'typography__paragraph',
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </p>
+  );
+};
+
+Paragraph.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+Paragraph.defaultProps = {
+  children: null,
+  className: null,
+};
+
+export default Paragraph;
