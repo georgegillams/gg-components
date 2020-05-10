b5807d274e81dc785f44fdcbf03ef209e421d9a4
George Gillams <george.gillams@skyscanner.net>
2020-05-10 08:10:45 +0100

    [NO-JIRA] Add first focus to page title

--- src/Typography/PageTitle.js
+++ src/Typography/PageTitle.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Section from './Section';
 import TextLink from './TextLink';
@@ -8,27 +8,39 @@ import STYLES from './page-title.scss';
 
 const getClassName = cssModules(STYLES);
 
-const PageTitle = props => {
-  const { link, ...rest } = props;
-
-  const sectionClassNames = [getClassName('page-title__section')];
-
-  if (link) {
-    sectionClassNames.push(getClassName('page-title__section--with-link'));
+class PageTitle extends Component {
+  componentDidMount() {
+    this.firstFocus.focus();
   }
 
-  return (
-    <Fragment>
-      {link && (
-        <TextLink
-          className={getClassName('page-title__link')}
-          to={link.to}
-        >{`⇠ ${link.text}`}</TextLink>
-      )}
-      <Section textClassName={sectionClassNames.join(' ')} {...rest} />
-    </Fragment>
-  );
-};
+  render() {
+    const { link, ...rest } = props;
+
+    const sectionClassNames = [getClassName('page-title__section')];
+
+    if (link) {
+      sectionClassNames.push(getClassName('page-title__section--with-link'));
+    }
+
+    return (
+      <Fragment>
+        {link && (
+          <TextLink
+            className={getClassName('page-title__link')}
+            to={link.to}
+          >{`⇠ ${link.text}`}</TextLink>
+        )}
+        <Section
+          ref={ref => {
+            this.firstFocus = ref;
+          }}
+          textClassName={sectionClassNames.join(' ')}
+          {...rest}
+        />
+      </Fragment>
+    );
+  }
+}
 
 PageTitle.propTypes = {
   name: PropTypes.string,
