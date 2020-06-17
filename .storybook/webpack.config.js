662b89d2618737c3cad1aaaef31e67c7e84dd9c8
George Gillams <george.gillams@skyscanner.net>
2020-06-17 08:13:19 +0100

    [NO-JIRA] Remove bpk-mixins (#246)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         use: ['babel-loader'],
-        exclude: /node_modules\/(?!bpk-).*/,
+        exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
