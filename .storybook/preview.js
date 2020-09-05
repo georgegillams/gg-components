dc6e83dad6acbb154cc513e40033ec7617f203f9
George Gillams <george.gillams@skyscanner.net>
2020-09-05 12:48:50 +0100

    [NO-JIRA] Upgrade storybook (#425)
    * [NO-JIRA] Upgrade storybook
    
    * Snapshot changes
    
    Co-authored-by: georgegillams <georgegillams@users.noreply.github.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import { addDecorator, addParameters } from '@storybook/react';
+export const decorators = [
+  Story => (
+    <div style={{ fontFamily: 'Quattrocento Sans' }}>
+      <Story />
+    </div>
+  ),
+];
 
-addDecorator(storyFn => {
-  return <div style={{ fontFamily: 'Quattrocento sans' }}>{storyFn()}</div>;
-});
-
-addParameters({
-  backgrounds: [
-    { name: 'Dark', value: '#000000' },
-    { name: 'Skyscanner', value: '#0770E3' },
-    { name: 'Orange', value: '#ffbb00' },
-  ],
-});
+export const parameters = {
+  backgrounds: {
+    values: [
+      { name: 'Dark', value: '#000000' },
+      { name: 'Skyscanner', value: '#0770E3' },
+      { name: 'Orange', value: '#ffbb00' },
+    ],
+  },
+};
